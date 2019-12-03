8bd105e073c4adb61f76d29d8ef96546cfe0b307
Carter <cdw77@drexel.edu>
2019-12-03 14:33:37 -0500

    Bigger image size and color images

--- implementations/acgan/acgan.py
+++ implementations/acgan/acgan.py
@@ -25,8 +25,8 @@ parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of firs
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
 parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
-parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
 opt = parser.parse_args()
 print(opt)
@@ -131,7 +131,7 @@ dataloader = torch.utils.data.DataLoader(
     datasets.ImageFolder(
         "../../data/pizzaGANdata",
         transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.CenterCrop(opt.img_size), transforms.Grayscale(num_output_channels=1), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+            [transforms.Resize((opt.img_size, opt.img_size)), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
         ),
     ),
     batch_size=opt.batch_size,
